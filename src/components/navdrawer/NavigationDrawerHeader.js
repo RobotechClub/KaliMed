0cb07fcc1fef8416556c64ec35eca4eb76bb8833
Mahmoud <mfenej@dtp.ae>
2020-01-13 20:03:45 +0200

    [NavigationHeader]: Add needed components to Navigation Header

--- src/components/navdrawer/NavigationDrawerHeader.js
+++ src/components/navdrawer/NavigationDrawerHeader.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import { DrawerItems } from 'react-navigation-drawer'
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { SearchBar } from 'react-native-elements';
+import { DrawerItems } from 'react-navigation-drawer';
+import SwitchButton from './SwitchButton';
 
 export const NavigationDrawerHeader = (props) => (
     <View>
-        <View
-            style={{
-                backgroundColor: '#f50057',
-                height: 140,
-                alignItems: 'center',
-                justifyContent: 'center',
-            }}>
-            <Text style={{ color: 'white', fontSize: 30 }}>
-                Header
-        </Text>
+        <View>
+            <Text> KaliMed </Text>
+            <Image style={{width: 80, height: 80}}  source={require('../../../images/logo.png')} />
+
+            <SearchBar
+                placeholder="Type Here..."
+                // onChangeText={this.updateSearch}
+                // value={search}
+            />
+
+            <SwitchButton switchWidth={150} ></SwitchButton>
         </View>
         <DrawerItems {...props} />
     </View>
 )
+
