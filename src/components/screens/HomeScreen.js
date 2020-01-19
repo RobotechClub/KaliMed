8da6d71216261d28fa33830c8897a204f732c4e5
Mahmoud <mfenej@dtp.ae>
2020-01-19 13:45:57 +0200

    Home Screen Styling

--- src/components/screens/HomeScreen.js
+++ src/components/screens/HomeScreen.js
@@ -9,10 +9,29 @@ const styles = StyleSheet.create({
     paddingTop: 22
   },
   item: {
-    padding: 10,
+    borderWidth: 2,
+    borderStyle: "solid",
+    backgroundColor: "#00bcd4",
+    borderColor: "#D3D3D3",
+    borderRadius: 10,
+    padding: 20,
     fontSize: 18,
-    height: 44,
+    marginLeft: 6,
+    marginTop: 7,
+    height: 60,
+    width: "95%",
   },
+  mainText: {
+    fontSize: 25,
+    textAlign: "center",
+    color: "#00bcd4",
+    fontStyle:"italic",
+    fontWeight:"bold",
+
+  },
+  listContainer: {
+    marginTop: 10
+  }
 })
 
 class HomeScreen extends React.Component {
@@ -20,24 +39,27 @@ class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <FlatList
-          data={[
-            { key: 'Devin' },
-            { key: 'Dan' },
-            { key: 'Dominic' },
-            { key: 'Jackson' },
-            { key: 'James' },
-            { key: 'Joel' },
-            { key: 'John' },
-            { key: 'Jillian' },
-            { key: 'Jimmy' },
-            { key: 'Julie' },
-          ]}
-          renderItem={({ item }) => <Text onPress={() => this.props.navigation.navigate('Details')} style={styles.item}>{item.key}</Text>}
-        />
-        <FloatingButton/>
-        <BookmarksDialog/>
+        <Text style={styles.mainText}> Anatomy </Text>
+        <View style={styles.listContainer}>
+          <FlatList
+            data={[
+              { key: 'Devin' },
+              { key: 'Dan' },
+              { key: 'Dominic' },
+              { key: 'Jackson' },
+              { key: 'James' },
+              { key: 'Joel' },
+              { key: 'John' },
+              { key: 'Jillian' },
+              { key: 'Jimmy' },
+              { key: 'Julie' },
+            ]}
+            renderItem={({ item }) => <Text onPress={() => this.props.navigation.navigate('Details')} style={styles.item}>{item.key}</Text>}
+          />
         </View>
+        <FloatingButton />
+        <BookmarksDialog />
+      </View>
 
     );
   }
