99582a69bfe443c1361aa0fb8247906ae2a32fd3
Mahmoud <mfenej@dtp.ae>
2020-01-19 15:32:02 +0200

    Navigation Drawer Header styling

--- src/components/navdrawer/NavigationDrawerHeader.js
+++ src/components/navdrawer/NavigationDrawerHeader.js
@@ -1,22 +1,63 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import { DrawerItems } from 'react-navigation-drawer';
 import SwitchButton from './SwitchButton';
 
+const styles = StyleSheet.create({
+    mainText: {
+        fontSize: 25,
+        color: "white",
+        fontWeight: "bold",
+        paddingLeft: 10,
+        paddingTop: 20
+
+    },
+    switchButton: {
+        paddingLeft: 30,
+        paddingTop: 10
+    },
+
+})
 export const NavigationDrawerHeader = (props) => (
     <View>
-        <View>
-            <Text> KaliMed </Text>
-            <Image style={{width: 80, height: 80}}  source={require('../../../images/logo.png')} />
+        <View style={{ backgroundColor: '#00bcd4' }}>
+            <View style={{ flexDirection: 'row' }}>
+                <Text style={styles.mainText}> KaliMed </Text>
+                <View style={{ width: '60%' }}>
+                    <SearchBar
+                        round={true}
+                        lightTheme={true}
+                        containerStyle={{
+                            backgroundColor: '#00bcd4',
+                            borderWidth: 0, //no effect
+                            shadowColor: 'white', //no effect
+                            borderBottomColor: 'transparent',
+                            borderTopColor: 'transparent'
+                        }}
+                        inputContainerStyle={{
+                            backgroundColor: '#00bcd4',
+                            borderWidth: 3,
+                            borderColor: 'yellow',
+                            borderBottomColor: 'yellow',
+                            borderBottomWidth: 3,
+                            borderTopColor: 'yellow',
+                            borderTopWidth: 3,
 
-            <SearchBar
-                placeholder="Type Here..."
-                // onChangeText={this.updateSearch}
-                // value={search}
-            />
+                        }}
+                        placeholder="Search"
+                        onChangeText={(e) => console.log(e)}
+                        value={''}
+                    />
+                </View>
+            </View>
+            <View style={{ flexDirection: 'row' }}>
+                <Image style={{ width: 80, height: 70, marginLeft: 10, marginBottom: 10 }} source={require('../../../images/logo.jpg')} />
+                <View style={styles.switchButton}>
+                    <SwitchButton switchWidth={150} ></SwitchButton>
+                </View>
 
-            <SwitchButton switchWidth={150} ></SwitchButton>
+            </View>
         </View>
         <DrawerItems {...props} />
     </View>
