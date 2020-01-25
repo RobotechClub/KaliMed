733175c5c3243613ba4fdab9de7f40e1849203f0
Mahmoud <mfenej@dtp.ae>
2020-01-25 17:29:21 +0200

    Refactor Nav Drawer Class

--- src/components/navdrawer/CustomSideBarMenu.js
+++ src/components/navdrawer/CustomSideBarMenu.js
@@ -14,56 +14,55 @@ export default class CustomSideBarMenu extends Component {
     }
     render() {
         return (
-            <View>
-                <NavigationDrawerHeader />
-                <View />
-                <ScrollView style={{ width: '100%' }}>
+            <ScrollView style={{ width: '100%' }}>
+                <View>
+                    <NavigationDrawerHeader />
+                    <View />
                     <Text style={{ marginTop: 20, marginRight: 2, marginLeft: 20, color: 'grey', fontSize: 18 }}>Systems</Text>
                     {this.items.map((item, key) => (
-                        <View
-                            style={{
-                                flexDirection: 'row',
-                                alignItems: 'center',
-                                paddingTop: 10,
-                                paddingBottom: 10,
-                                backgroundColor: global.currentScreenIndex === key ? '#e0dbdb' : '#ffffff',
-                            }}
-                            key={key}>
-                            <View style={{ marginRight: 2, marginLeft: 20 }}>
-                            </View>
-                            <Text
-                                style={{
-                                    fontSize: 15,
-                                    color: global.currentScreenIndex === key ? '#00bcd4' : 'black',
-                                }}
-                                onPress={() => {
-                                    global.currentScreenIndex = key;
-                                    this.props.navigation.navigate('Home');
-                                    this.props.navigation.closeDrawer();
-                                }}>
-                                {item}
-                            </Text>
-                        </View>
+                        this.getRowItem(key, item, (key) => this.homePressAction(key))
                     ))}
-                    <View
-                        style={{
-                            borderBottomColor: 'grey',
-                            borderBottomWidth: 1,
-                        }}
-                    />
+                    {this.getSeparatorView()}
                     <Text style={{ marginTop: 20, marginRight: 2, marginLeft: 20, color: 'grey', fontSize: 18 }}>Notifications</Text>
-
-                    <View
-                        style={{
-                            borderBottomColor: 'grey',
-                            borderBottomWidth: 1,
-                        }}
-                    />
+                    {this.getRowItem(this.items.length + 1, "Term of the Day!", (key) => this.homePressAction(key))}
+                    {this.getSeparatorView()}
                     <Text style={{ marginTop: 20, marginRight: 2, marginLeft: 20, color: 'grey', fontSize: 18 }}>Contact Us</Text>
-
-                </ScrollView>
-            </View>
+                    {this.getRowItem(this.items.length + 2, "Send Email", (key) => this.homePressAction(key))}
+                </View>
+            </ScrollView>
         );
+    }
+
+    getSeparatorView() {
+        return <View style={{
+            borderBottomColor: 'grey',
+            borderBottomWidth: 1,
+        }} />;
+    }
+
+    getRowItem(key, item, homePressAction) {
+        return <View style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            paddingTop: 10,
+            paddingBottom: 10,
+            backgroundColor: global.currentScreenIndex === key ? '#e0dbdb' : '#ffffff',
+        }} key={key}>
+            <View style={{ marginRight: 2, marginLeft: 20 }}>
+            </View>
+            <Text style={{
+                fontSize: 15,
+                color: global.currentScreenIndex === key ? '#00bcd4' : 'black',
+            }} onPress={() => homePressAction(key)}>
+                {item}
+            </Text>
+        </View>;
+    }
+
+    homePressAction(key) {
+        global.currentScreenIndex = key;
+        this.props.navigation.navigate('Home');
+        this.props.navigation.closeDrawer();
     }
 }
 const styles = StyleSheet.create({
